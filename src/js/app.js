5f353616feb54fc78d3bd7c186413beff35a899e
Alexander <kanashkin_ad@mail.ru>
2024-05-04 16:43:03 +0300

    ready contact block

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
 import './modules/isWebp.js'
 import tabs from './modules/tabs.js';
 import trustSwiper from './modules/trustSwiper.js';
+import IMask from 'imask';
+import phoneMask from './modules/phoneMask.js';
 
 window.addEventListener('DOMContentLoaded', function() {
     tabs('.services__tab', '.services__content')
     trustSwiper()
+    phoneMask()
 })
