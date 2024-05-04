69944f5db0223b27f33356d81c6a259ea835baea
Alexander <kanashkin_ad@mail.ru>
2024-05-04 13:32:17 +0300

    ready trust block

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import './modules/isWebp.js'
 import tabs from './modules/tabs.js';
+import trustSwiper from './modules/trustSwiper.js';
 
 window.addEventListener('DOMContentLoaded', function() {
     tabs('.services__tab', '.services__content')
+    trustSwiper()
 })
---
+++ src/js/modules/trustSwiper.js
@@ -0,0 +1,18 @@
+function trustSwiper() {
+    const swiperEl = document.querySelector('.trust__swiper')
+
+    if (swiperEl) {
+        const swiper = new Swiper(swiperEl, {
+            spaceBetween: 24,
+            loop: true,
+            speed: 800,
+            slidesPerView: 'auto',
+            navigation: {
+                prevEl: '.trust__swiper-left',
+                nextEl: '.trust__swiper-right'
+            }
+        })
+    }
+}
+
+export default trustSwiper
