8ed9ae04aaa00a5bb4fd7c8e57d5c4609cfa809c
Alexander <kanashkin_ad@mail.ru>
2024-05-21 21:32:48 +0300

    add scroll animation

--- src/js/modules/currentMenuItem.js
+++ src/js/modules/currentMenuItem.js
@@ -1,7 +1,6 @@
 function currentMenuItem(menuItemsClass) {
     const menuItems = document.querySelectorAll(menuItemsClass)
     const currentPageIndex = document.querySelector('main').getAttribute('page-index')
-    console.log(menuItems);
 
     if (menuItems && currentPageIndex) {
         menuItems[currentPageIndex - 1].classList.add('active')
